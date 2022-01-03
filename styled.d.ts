3777225b8a67f9869e1522297f04356c8fc12b05
cedgo <cedgo1997@gmail.com>
2022-01-03 00:17:18 -0400

    add: style declaration

---
+++ styled.d.ts
@@ -0,0 +1,30 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+        primary: {
+            dark: string,
+            light: string
+        },
+        secondary: {
+            dark: string,
+            normal: string,
+            light: string,
+            lightest: string
+        }
+    },
+    fontSize: {
+        small: string,
+        normal: string,
+        large: string
+    },
+    fontFamily: {
+        light: string,
+        normal: string,
+        bold: string
+    }
+  }
+}
