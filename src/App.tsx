538f12c824bb13975517c58fed9f5b0e8f48cf40
cedgo <cedgo1997@gmail.com>
2022-01-04 02:58:11 -0400

    Apply navbar globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Navbar } from './components/Navbar/Navbar'
 
 export const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <p>App</p>
+    </div>
   )
 }
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import { App } from "../App";
+import { Navbar } from "../components/Navbar/Navbar";
 import { AboutScreen } from "../screens/About/AboutScreen";
 import { ContactScreen } from "../screens/Contact/ContactScreen";
 import { ProductScreen } from "../screens/Product/ProductScreen";
@@ -9,6 +10,7 @@ import { ProductsScreen } from "../screens/Products/ProductsScreen";
 export const AppRouter = () => {
     return (
         <div>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/collections" element={<ProductsScreen />} />
