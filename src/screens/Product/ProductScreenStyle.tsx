00712c4706e46975217588d9221e0ed9e59f1022
cedgo <cedgo1997@gmail.com>
2022-01-04 19:29:18 -0400

    Add: first styling components

---
+++ src/screens/Product/ProductScreenStyle.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+
+/* IMAGES */
+export const ProductContainer = styled.div`
+    display: flex;
+    margin-top: 5%;
+    margin-left: 5%;
+`
+
+export const ProductImageContainer = styled.div``
+
+export const Image = styled.img`
+    width: 75%;
+    height: 80%;
+    border-radius: 5%;
+`
+
+export const ProductThumbnailContainer = styled.div``
+
+export const ProductThumbnail = styled.img`
+    border-radius: 10%;
+    width: 15%;
+    height: 15%;
+    margin-right: 5%;
+    margin-top: 3%;
+`
+
+/* PRODUCT DATA */
+export const DataContainer = styled.div``
