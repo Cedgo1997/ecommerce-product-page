f95cec4cca352c5bdc2ad856705671fa666188b8
cedgo <cedgo1997@gmail.com>
2022-01-11 21:45:22 -0400

    add: useEffect to change price dinamically

--- src/screens/Product/ProductScreen.tsx
+++ src/screens/Product/ProductScreen.tsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AddButton, BrandName, ButtonsContainer, CurrentPrice, DataContainer, DiscountBadge, Image, LargeImageContainer, PreviousPrice, Price, PriceContainer, ProductContainer, ProductDescription, ProductImageContainer, ProductThumbnail, ProductThumbnailContainer, ProductTitle, QuantityButton, Sign } from './ProductScreenStyle'
 
 export const ProductScreen: React.FC = () => {
 
-    const [quantity, setQuantity] = useState(0);
+    const [quantity, setQuantity] = useState(1);
 
+    const [productData, setProductData] = useState({
+        name: 'Fall Limited Edition Sneakers',
+        price: 125.00,
+        quantity
+    });
+
+    useEffect(() => {
+        setProductData((state) => {
+            return {
+                ...state,
+                quantity
+            }
+        })
+    }, [quantity]);
 
     return (
         <ProductContainer>
@@ -21,7 +35,7 @@ export const ProductScreen: React.FC = () => {
             </ProductImageContainer>
             <DataContainer>
                 <BrandName>SNEAKER COMPANY</BrandName>
-                <ProductTitle>Fall Limited Edition Sneakers</ProductTitle>
+                <ProductTitle>{productData.name}</ProductTitle>
                 <ProductDescription>
                     These low-profile sneakers are your perfect  casual wear companion.
                     Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.
@@ -29,15 +43,15 @@ export const ProductScreen: React.FC = () => {
 
                 <PriceContainer>
                     <CurrentPrice>
-                        <Price>$125.00</Price>
+                        <Price>{productData.price * productData.quantity}</Price>
                         <DiscountBadge>50%</DiscountBadge>
                     </CurrentPrice>
-                    <PreviousPrice>$250.00</PreviousPrice>
+                    <PreviousPrice>{(productData.price * 2)*productData.quantity}</PreviousPrice>
                 </PriceContainer>
 
                 <ButtonsContainer>
                     <QuantityButton>
-                        <Sign text='-' onClick={() => !!quantity && setQuantity(quantity - 1)} />
+                        <Sign text='-' onClick={() => !!(quantity - 1) && setQuantity(quantity - 1)} />
                         <p className="number">{quantity}</p>
                         <Sign text='+' onClick={() => setQuantity(quantity + 1)} />
                     </QuantityButton>
