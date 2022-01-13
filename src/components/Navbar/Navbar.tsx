5ef032117982c1218e478ec30fe2fc51bb20dae2
cedgo <cedgo1997@gmail.com>
2022-01-12 21:33:10 -0400

    Add: Side menu buttons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,40 +1,41 @@
 import React, { useState } from 'react'
 import { Cart } from '../Cart/Cart'
-import { CartButton, LogoContainer, MainLogo, Nav, NavItem, NavItems, ProfileButton, ProfileContainer, StyledLink } from './NavbarStyle'
+import { CartButton, CloseButton, LogoContainer, MainLogo, MenuButton, Nav, NavItem, NavItems, ProfileButton, ProfileContainer, StyledLink } from './NavbarStyle'
 
-export const Navbar = () => {
+export const Navbar: React.FC = () => {
 
     const [showCart, setShowCart] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
     return (
-        <>
-            <Nav>
-                {showCart && (<Cart />)}
-                <LogoContainer>
-                    <MainLogo />
-                </LogoContainer>
-                <NavItems>
-                    <NavItem>
-                        <StyledLink to="/collections" >Collections</StyledLink>
-                    </NavItem>
-                    <NavItem>
-                        <StyledLink to="/men" >Men</StyledLink>
-                    </NavItem>
-                    <NavItem>
-                        <StyledLink to="/women" >Women</StyledLink>
-                    </NavItem>
-                    <NavItem>
-                        <StyledLink to="/about" >About</StyledLink>
-                    </NavItem>
-                    <NavItem>
-                        <StyledLink to="/contact" >Contact</StyledLink>
-                    </NavItem>
-                </NavItems>
-                <ProfileContainer>
-                    <CartButton  onClick={() => setShowCart(!showCart)}/>
-                    <ProfileButton src={require('./../../assets/images/image-avatar.png')} width={35} height={35} />
-                </ProfileContainer>
-            </Nav>
-        </>
+        <Nav>
+            {showCart && (<Cart />)}
+            <MenuButton onClick={() => setShowMenu(!showMenu)} />
+            <LogoContainer>
+                <MainLogo />
+            </LogoContainer>
+            <NavItems showMenu={showMenu}>
+                <CloseButton onClick={() => setShowMenu(!showMenu)} />
+                <NavItem>
+                    <StyledLink to="/collections" >Collections</StyledLink>
+                </NavItem>
+                <NavItem>
+                    <StyledLink to="/men" >Men</StyledLink>
+                </NavItem>
+                <NavItem>
+                    <StyledLink to="/women" >Women</StyledLink>
+                </NavItem>
+                <NavItem>
+                    <StyledLink to="/about" >About</StyledLink>
+                </NavItem>
+                <NavItem>
+                    <StyledLink to="/contact" >Contact</StyledLink>
+                </NavItem>
+            </NavItems>
+            <ProfileContainer>
+                <CartButton onClick={() => setShowCart(!showCart)} />
+                <ProfileButton src={require('./../../assets/images/image-avatar.png')} width={35} height={35} />
+            </ProfileContainer>
+        </Nav>
     )
 }
