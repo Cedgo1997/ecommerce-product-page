7bec17611e679559afbbc84587832b62670bc157
cedgo <cedgo1997@gmail.com>
2022-01-14 19:21:04 -0400

    Render images dinamically: Define a images const with image path and thumbnails. Map images to get image component with images info. add onclick action to change large image while clicking.

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 import { Image, LargeImageContainer, ProductImageContainer, ProductThumbnail, ProductThumbnailContainer } from './SliderStyle'
 
@@ -6,17 +6,45 @@ export const Slider: React.FC = () => {
 
     const { width } = useWindowDimensions();
 
+    const images = [
+        {
+            path: require('./../../assets/images/image-product-1.jpg'),
+            thumbnail: require('./../../assets/images/image-product-1-thumbnail.jpg'),
+            alt: 'product 1'
+        },
+        {
+            path: require('./../../assets/images/image-product-2.jpg'),
+            thumbnail: require('./../../assets/images/image-product-2-thumbnail.jpg'),
+            alt: 'product 2'
+        },
+        {
+            path: require('./../../assets/images/image-product-3.jpg'),
+            thumbnail: require('./../../assets/images/image-product-3-thumbnail.jpg'),
+            alt: 'product 3'
+        },
+        {
+            path: require('./../../assets/images/image-product-4.jpg'),
+            thumbnail: require('./../../assets/images/image-product-4-thumbnail.jpg'),
+            alt: 'product 4'
+        }
+    ];
+
+    const [image, setImage] = useState(images[0].path);
+
     return (
         <ProductImageContainer>
-            <LargeImageContainer>
-                <Image src={require('./../../assets/images/image-product-1.jpg')} alt="product 1" />
-            </LargeImageContainer>
-            {width >= 500 && <ProductThumbnailContainer>
-                <ProductThumbnail src={require('./../../assets/images/image-product-1-thumbnail.jpg')} alt="product 1" />
-                <ProductThumbnail src={require('./../../assets/images/image-product-2-thumbnail.jpg')} alt="product 2" />
-                <ProductThumbnail src={require('./../../assets/images/image-product-3-thumbnail.jpg')} alt="product 3" />
-                <ProductThumbnail src={require('./../../assets/images/image-product-4-thumbnail.jpg')} alt="product 4" />
-            </ProductThumbnailContainer>}
+            {width >= 500 &&
+                <>
+                    <LargeImageContainer>
+                        <Image src={image} alt="product 1" />
+                    </LargeImageContainer>
+                    <ProductThumbnailContainer>
+                        {
+                            images.map((image, index) => <ProductThumbnail src={image.thumbnail} alt={image.alt} key={index} onClick={() => setImage(image.path)} />)
+                        }
+                    </ProductThumbnailContainer>
+                </>
+            }
         </ProductImageContainer>
     )
 }
