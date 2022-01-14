cb0ba0a40c2af64fce865dc79f518af66c8aa3ef
cedgo <cedgo1997@gmail.com>
2022-01-14 19:22:07 -0400

    Separate product images from component.

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,33 +1,11 @@
 import React, { useState } from 'react'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 import { Image, LargeImageContainer, ProductImageContainer, ProductThumbnail, ProductThumbnailContainer } from './SliderStyle'
+import { images } from './images';
 
 export const Slider: React.FC = () => {
 
     const { width } = useWindowDimensions();
-
-    const images = [
-        {
-            path: require('./../../assets/images/image-product-1.jpg'),
-            thumbnail: require('./../../assets/images/image-product-1-thumbnail.jpg'),
-            alt: 'product 1'
-        },
-        {
-            path: require('./../../assets/images/image-product-2.jpg'),
-            thumbnail: require('./../../assets/images/image-product-2-thumbnail.jpg'),
-            alt: 'product 2'
-        },
-        {
-            path: require('./../../assets/images/image-product-3.jpg'),
-            thumbnail: require('./../../assets/images/image-product-3-thumbnail.jpg'),
-            alt: 'product 3'
-        },
-        {
-            path: require('./../../assets/images/image-product-4.jpg'),
-            thumbnail: require('./../../assets/images/image-product-4-thumbnail.jpg'),
-            alt: 'product 4'
-        }
-    ];
 
     const [image, setImage] = useState(images[0].path);
 
