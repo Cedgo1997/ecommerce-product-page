50595ed026e2d608ca898607138a4d0904d6b72e
cedgo <cedgo1997@gmail.com>
2022-01-14 20:29:22 -0400

    add: show modal state to control modal

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,20 +2,23 @@ import React, { useState } from 'react'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 import { Image, LargeImageContainer, ProductImageContainer, ProductThumbnail, ProductThumbnailContainer } from './SliderStyle'
 import { images } from './images';
+import { Modal } from './Modal/Modal';
 
 export const Slider: React.FC = () => {
 
     const { width } = useWindowDimensions();
-
     const [image, setImage] = useState(images[0].path);
-
     const [isActive, setIsActive] = useState(0);
+    const [showModal, setShowModal] = useState(false);
 
     return (
         <ProductImageContainer>
+            {
+                showModal && <Modal />
+            }
             {width >= 500 &&
                 <>
-                    <LargeImageContainer>
+                    <LargeImageContainer onClick={() => setShowModal(true)}>
                         <Image src={image} alt="product 1" />
                     </LargeImageContainer>
                     <ProductThumbnailContainer>
